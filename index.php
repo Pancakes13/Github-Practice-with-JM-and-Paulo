87797087a32eb14ca961afdf2643c5d4e935ef28
Pancakes13 <neil.llenes@gmail.com>
2017-06-28 18:58:49 +0800

    Fixed links
    fklsajdf

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head>
+<head>  
+  <link rel="stylesheet" href="includes/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="includes/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="includes/ionicons-2.0.1/css/ionicons.min.css">
+  <link rel="shortcut icon" href="includes/img/universaltechlogo2.jpg" />
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<style>
 		#infoTable{
 			background-color:lightblue;
@@ -38,8 +43,10 @@
 
 	</table>
 </div>
-	<button onclick="unHide()"> Click Me! </button>
+	<button class="btn btn-primary" onclick="unHide()"> Click Me! </button>
 </body>
+<script src="includes/jQuery/jquery-3.2.1.min.js"></script>
+<script src="includes/bootstrap/js/bootstrap.min.js"></script>  
 <script>
 function unHide() {
     var x = document.getElementById('taable');
