87e28adee6292f284c31c865b85973163b1e6cbf
Pancakes13 <neil.llenes@gmail.com>
2017-06-28 18:20:11 +0800

    Created an index page
    Made a table(With my personal info) in the created index page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+	<h1>Welcome to the Github practice boys</h1>
+	<table>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Age</th>
+				<th>email</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Neil</td>
+				<td>19</td>
+				<td>neil.llenes@gmail.com_addref</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
